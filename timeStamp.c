74f282c6122ed45d7c90a51c052e363ec8f2f3b5
Nick Jones <tsip_67@yahoo.com>
2015-11-20 20:46:20 -0500

    update to timeStamp.c
    uses time.h instead

---
+++ timeStamp.c
@@ -0,0 +1,30 @@
+// timeStamp.c for wSQL
+
+// This creates a timestamp from the system time. Looking at time.h for a better alternative
+
+// includes
+#include "nick.h"
+#include <string.h>
+#include <time.h>
+
+// defines
+
+// structs
+
+
+// prototypes
+
+
+char *timeStamp()
+{
+	time_t rawtime;
+	struct tm timeinfo;
+
+	time(&rawtime);
+	localtime_s(&timeinfo, &rawtime);
+
+	strftime(time_stamp, 26, "date (%m%d%Y) %r", &timeinfo);
+	puts(time_stamp);
+
+	return time_stamp;
+} // end timeStamp()
