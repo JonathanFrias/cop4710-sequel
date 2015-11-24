7f4b0cabff6b3ed155d2fc0b6d797e5b02db5ad5
Stephen Costello <n00176094@osprey.unfcsd.unf.edu>
2015-11-24 13:05:50 -0500

    Changes to sql.c to make it our opening. you might like what the banner does now. HOWEVER!! INFINITE LOOP CURRENTLY WITH ANY INPUT!!
    you are warned...

--- src/sql.c
+++ src/sql.c
@@ -1,8 +1,43 @@
 #include "common.h"
 
-void SqlRun() 
+int main(void)
 {
-  printf("\nHello World!\n");
+	int i = 0;
+	srand(1/2);
+	char * sqlLine =   
+		("                                                                      \n"
+		 "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"
+		 "::******************************************************************::\n"
+		 "::**                                                              **::\n"
+		 "::**                   SSSSSS       QQQQQQQ     LL                **::\n"
+		 "::**                  SS     SS    QQ     QQ    LL                **::\n"
+		 "::**                  SS          QQ       QQ   LL                **::\n"
+		 "::**  ww       ww      SSSSSS     QQ       QQ   LL        xx  xx  **::\n"
+		 "::**   w       w            SS    QQ     Q QQ   LL          xx    **::\n"
+		 "::**    w  w  w      SS     SS     QQ     QQ    LL          xx    **::\n"
+		 "::**     ww ww         SSSSSS       QQQQQQQ Q   LLLLLLL   xx  xx  **::\n"
+		 "::**                                                              **::\n"
+		 "::******************************************************************::\n"
+		 "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"
+		 "                                                                      \n");
 
-  getchar();
+	for (i = 0; i < strlen(sqlLine); i++)
+	{
+		printf("%c", sqlLine[i]);
+		usleep(10000);
+	}
+
+	printf("%45s\n", "Welcome to wSQLx!");
+
+
+	char * sqlString = "wSQLx: ";
+	char * input = {0};
+	do
+	{
+		printf("%sPlese input command.", sqlString);
+		scanf("%s", input);
+
+
+	}while (input != "exit");    //this infinitely loops
 }
+
