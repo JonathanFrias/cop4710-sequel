8f3d82e72985031f6b0ccbbbe79aa98a94da9f5c
Nick Jones <tsip_67@yahoo.com>
2015-11-27 15:37:42 -0500

    Update sql.c

--- sql.c
+++ sql.c
@@ -1,14 +1,10 @@
-#include "sql.tab.h"
-#include "lexer.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "common.h"
 
-int main(void)
+int main(void) 
 {
-	int i = 0;
-	srand(1/2);
-	char * sqlLine =   
+	int i = 0; 
+	srand(1/2); 
+	char * sqlLine =    
 		("                                                                      \n"
 		 "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n"
 		 "::******************************************************************::\n"
@@ -27,36 +23,34 @@ int main(void)
 
 	for (i = 0; i < strlen(sqlLine); i++)
 	{
-		printf("%c", sqlLine[i]);
+		printf("%c", sqlLine[i]); 
 		usleep(2000);
 	}
 
 	printf("Welcome to wSQLx! Enter a command or\n");
 	printf("Enter 'exit' to quit wSQLx or 'help' for more information\n\n");
 
-	yyscan_t scanner;
-	printf("scanner created\n");
-	YY_BUFFER_STATE buf;
-	printf("buf created\n");
-	yylex_init(&scanner);
-	printf("scanner initialized\n");
+	extern int yyparse(void);
+	char user_input[300];
+	char temp[300];
 	
-	char * user_input = {0};
-	printf("user_input initialized\n");
-	//do
-	//{
+	do 
+	{
 		printf("wSQLx: ");
 		scanf("%[^\n]%*c", user_input);
 		
-		buf = yy_scan_string(user_input, scanner);
-		yyparse(user_input);
+		
+		user_input[strlen(user_input)+1] = '\0';
+		strcpy(temp, user_input);
+		YY_BUFFER_STATE string_buffer = yy_scan_string(temp);
+		yy_switch_to_buffer(string_buffer);
+		yyparse();
+		yy_delete_buffer(string_buffer);
 		
 
 
-	//}while (strcmp(user_input, "exit") != 0);    //this infinitely loops
+	}while (strcmp(user_input, "exit") != 0);    //this infinitely loops
 	
-	yy_delete_buffer(buf, scanner);
-    yylex_destroy(scanner);
 	
 	return 0;
 	
